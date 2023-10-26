0cbf81ed947de7680dcbd34b2a946d2f00b1f0d0
sopanha09 <sopanhasous@mail.com>
2023-10-26 19:20:52 +0700

    Node.js CRUD Operations with JWT

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const { join } = require("path");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successful"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Backend server is running!");
+});
---
+++ routes/auth.js
@@ -0,0 +1,66 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: await CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+    // console.log(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+    // console.log(err);
+  }
+});
+
+// LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.body.username,
+    });
+    if (!user) res.status(401).json("Wrong No User");
+
+    const inputPassword = req.body.password;
+
+    const hashedPassword = await CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    console.log(OriginalPassword, req.body.password);
+    if (OriginalPassword !== inputPassword)
+      res.status(401).json({ message: "Wrong Password!" });
+    else {
+      const accessToken = jwt.sign(
+        {
+          id: user._id,
+          isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SEC,
+        { expiresIn: "3d" }
+      );
+
+      const { password, ...others } = user._doc;
+
+      res.status(200).json({ data: others, accessToken });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Error " });
+  }
+});
+
+module.exports = router;
